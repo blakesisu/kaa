350b24c73d8207e0f6354682186831654f576dcf
Steven Waller <jstevenwaller@gmail.com>
2017-12-08 13:38:28 -0800

    Added grid section 3

--- site/src/index.php
+++ site/src/index.php
@@ -3,5 +3,6 @@
 <?php include("template-parts/home/hero.php"); ?>
 <?php include("template-parts/home/grid-1.php"); ?>
 <?php include("template-parts/home/grid-2.php"); ?>
+<?php include("template-parts/home/grid-3.php"); ?>
 
 <?php get_footer(); ?>
