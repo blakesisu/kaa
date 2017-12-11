58790c3a2e4b52478e0cbc009ce372a62db6a4fc
Blake Watkins <iamsoblake@gmail.com>
2017-12-11 15:10:58 -0800

    Create details template which includes template-parts

---
+++ site/src/details.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * details page template
+ * Template Name: details
+ * parts directory: template-parts/details
+ */
+get_header(); ?>
+
+<?php include("template-parts/details/hero.php"); ?>
+<?php include("template-parts/details/grid-1.php"); ?>
+<?php include("template-parts/details/grid-2.php"); ?>
+<?php include("template-parts/details/grid-3.php"); ?>
+<?php include("template-parts/details/grid-4.php"); ?>
+<?php include("template-parts/details/grid-5.php"); ?>
+<?php include("template-parts/details/grid-6.php"); ?>
+<?php include("template-parts/details/grid-7.php"); ?>
+
+<?php get_footer(); ?>
