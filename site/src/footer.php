5d6bc95a32766e3ed26f198ab1cb1c48f4746f5b
Steven Waller <jstevenwaller@gmail.com>
2017-12-12 16:15:34 -0800

    Footer styles. Create more useful functions and split them out into it's own file

--- site/src/footer.php
+++ site/src/footer.php
@@ -1,3 +1,5 @@
+  <?php include("template-parts/common/footer.php"); ?>
+
   <script src="<?php echo get_stylesheet_directory_uri().'/js/manifest.js'; ?>"></script>
   <script src="<?php echo get_stylesheet_directory_uri().'/js/vendor.js'; ?>"></script>
   <script src="<?php echo get_stylesheet_directory_uri().'/js/scripts.js'; ?>"></script>
