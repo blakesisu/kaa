9829c1c54ac1202399e49ae9a5e659f8e095b300
Blake Watkins <iamsoblake@gmail.com>
2017-12-13 13:26:26 -0800

    Fix text boxes and column ordering for mobile

--- site/src/template-parts/details/grid-1.php
+++ site/src/template-parts/details/grid-1.php
@@ -1,7 +1,7 @@
 <section class="details-grid-inline details-grid--1">
 
     <!-- Image Block -->
-    <div class="details-grid__horizontal">
+    <div class="details-grid__horizontal grid-item1">
       <picture class="details-image-block__picture">
         <source srcset="<?php bloginfo('template_url'); ?>/images/details/grid-1/details-frontdoor.jpg">
         <img class="details-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/details/grid-1/details-frontdoor.jpg" alt="">
@@ -9,7 +9,7 @@
     </div>
 
     <!-- Text Block -->
-    <div class="details-grid__horizontal">
+    <div class="details-grid__horizontal grid-item2">
       <div class="details-text-grid__1">
         <span>Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.</span>
       </div>
--- site/src/template-parts/details/grid-7.php
+++ site/src/template-parts/details/grid-7.php
@@ -1,12 +1,14 @@
 <section class="details-grid details-grid--7">
   <!-- Text Block -->
-  <div class="details-text-block__header-content">
-    <h1 class="details-text-mini__header">RELATED PROJECTS</h1>
+  <div class="details-header-container grid-item1">
+    <div class="details-text-block__header-content">
+      <h1 class="details-text-mini__header">RELATED PROJECTS</h1>
+    </div>
+    <div class="details-text-block__dash"></div>
   </div>
-  <div class="details-text-block__dash"></div>
 
     <!-- Image Block -->
-  <div class="details-grid-inline details-grid--7">
+  <div class="details-grid-inline details-grid--7 grid-item2">
     <div class="details-grid__horizontal">
       <picture class="details-image-block__picture">
         <source srcset="<?php bloginfo('template_url'); ?>/images/details/grid-7/details-poolhouse.jpg">
@@ -14,7 +16,7 @@
       </picture>
     </div>
 
-    <div class="details-grid__horizontal">
+    <div class="details-grid__horizontal grid-item3">
       <picture class="details-image-block__picture">
         <source srcset="<?php bloginfo('template_url'); ?>/images/details/grid-7/details-livingroom.jpg">
         <img class="details-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/details/grid-7/details-livingroom.jpg" alt="">
