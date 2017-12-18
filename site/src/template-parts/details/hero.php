fab993c1ecfcacfd877ee4ad17f9e80f07f08430
Blake Watkins <iamsoblake@gmail.com>
2017-12-18 15:03:51 -0800

    Get desktop styles set with new pic dims

--- site/src/template-parts/details/hero.php
+++ site/src/template-parts/details/hero.php
@@ -5,14 +5,6 @@
   </div>
   <div class="details-text-block__dash"></div>
 
-  <!-- Image Block -->
-  <div class="details-hero">
-    <div class="details-image-block">
-      <picture class="details-image-block__picture">
-        <source srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-sm.jpg" media="(max-width: 700px)">
-        <source srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg">
-        <img class="details-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg" alt="">
-      </picture>
-    </div>
-  </div>
+  <!-- Image Block (Background image) -->
+  <div class="details-hero"></div>
 </section>
