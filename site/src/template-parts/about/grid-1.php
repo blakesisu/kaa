451b5f9718f1c5e30912a8780e30f6e93564b06f
Blake Watkins <iamsoblake@gmail.com>
2017-12-19 14:18:01 -0800

    Create base templates for grid-1 and grid-2

---
+++ site/src/template-parts/about/grid-1.php
@@ -0,0 +1,8 @@
+<section class="about-grid-inline about-grid--2">
+
+    <!-- Text Block -->
+      <div class="about-text-grid__2">
+        <span>Welcome to KAA Design. Led by out founding Partner Grant Kirkpatrick, Partner Duan Tran, and Principal Patti Baker, we are a design studio that has been dedicated to advancing the California lifestyle through contemporary architecture since 1988.</span>
+      </div>
+
+</section>
---
+++ site/src/template-parts/about/grid-2.php
@@ -0,0 +1,77 @@
+<section class="about-grid about-grid--7">
+
+    <!-- Text Block -->
+    <div class="about-header-container grid-item1">
+      <div class="about-header-grid-7">
+        <h1 class="about-text-mini__header">VALUES OVER RULES</h1>
+      </div>
+    </div>
+
+    <!-- Image/Text Block -->
+    <div class="about-grid-column">
+      <div class="about-grid-inline about-grid--1">
+        <!-- Image Block -->
+        <div class="about-grid__horizontal grid-item1">
+          <picture class="about-image-block__picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-1-sm.jpg" media="(max-width: 700px)">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-1-lg.jpg">
+            <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-1-lg.jpg" alt="">
+          </picture>
+        </div>
+
+        <!-- Text Block TODO: check landing for layout inspiration on text blocks-->
+        <div class="about-grid__horizontal grid-item2">
+          <div class="about-text-grid__1">
+            <span>ARCHITECTURAL INTERGRITY &amp; AUTHENTICITY</span>
+            <div class="about-text-block__dash"></div>
+            <span>Morbi in sem quis dui placerat ornare, pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu cras neque id cursus faucibus consequat.</span>
+          </div>
+        </div>
+
+      </div>
+
+
+      <div class="about-grid-inline about-grid--1">
+        <!-- Image Block -->
+        <div class="about-grid__horizontal grid-item2">
+          <picture class="about-image-block__picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-2-sm.jpg" media="(max-width: 700px)">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-2-lg.jpg">
+            <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-2-lg.jpg" alt="">
+          </picture>
+        </div>
+
+        <!-- Text Block TODO: check landing for layout inspiration on text blocks-->
+        <div class="about-grid__horizontal grid-item1">
+          <div class="about-text-grid__1">
+            <span>EMBRACING DIFFERENT VOICES</span>
+            <div class="about-text-block__dash"></div>
+            <span>Phasellus ultrices nulla ques nibh. Quisque a lectus. Donectsum consectetuer ligula vulputate sem tristique cursus. nam nullants quam, gravida non, comodo a, sodales sit amet, nisi.</span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="about-grid-inline about-grid--1">
+        <!-- Image Block -->
+        <div class="about-grid__horizontal grid-item1">
+          <picture class="about-image-block__picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-3-sm.jpg" media="(max-width: 700px)">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-3-lg.jpg">
+            <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/grid-2/value-3-lg.jpg" alt="">
+          </picture>
+        </div>
+
+        <!-- Text Block TODO: check landing for layout inspiration on text blocks-->
+        <div class="about-grid__horizontal grid-item2">
+          <div class="about-text-grid__1">
+            <span>WORK HARD, PLAY HARDER</span>
+            <div class="about-text-block__dash"></div>
+            <span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</span>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+</section>
