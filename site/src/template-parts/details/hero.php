9c42cb8dd06008fef2244d59ef8ca610e8399a72
Steven Waller <jstevenwaller@gmail.com>
2017-12-19 14:45:54 -0800

    Refactored Project hero styles - Kept text and image styles in one scss file for easier reference - Simplified markup - Using inline image since the section's height is not based on viewport height

--- site/src/template-parts/details/hero.php
+++ site/src/template-parts/details/hero.php
@@ -1,10 +1,11 @@
-<section class="details-grid details-grid--0">
-  <!-- Text Block -->
-  <div class="details-text-block__header-content">
-    <h1 class="details-text-block__header">TOWER GROVE DRIVE</h1>
+<section class="details-hero">
+  <div class="details-hero__container">
+    <h1 class="details-hero__header">TOWER GROVE DRIVE</h1>
+    <div class="details-hero__dash"></div>
+    <picture class="details-hero__picture">
+      <source srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-sm.jpg" media="(max-width: 700px)">
+      <source srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg">
+      <img class="details-hero__img" srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg" alt="">
+    </picture>
   </div>
-  <div class="details-text-block__dash"></div>
-
-  <!-- Image Block (Background image) -->
-  <div class="details-hero"></div>
 </section>
