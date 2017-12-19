36d706d078321c67d9f8a9a7d84fe39c39b43b1f
Blake Watkins <iamsoblake@gmail.com>
2017-12-19 12:59:09 -0800

    Initial commit for about page

---
+++ site/src/about.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * about page template
+ * Template Name: about
+ * parts directory: template-parts/about
+ */
+get_header(); ?>
+
+<?php include("template-parts/about/hero.php"); ?>
+
+<?php get_footer(); ?>
