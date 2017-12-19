55b110a40f34bd6abe1ba76b97d7911cc4ec9b80
Blake Watkins <iamsoblake@gmail.com>
2017-12-19 14:17:20 -0800

    Move about images to correct directories

--- site/src/about.php
+++ site/src/about.php
@@ -7,5 +7,7 @@
 get_header(); ?>
 
 <?php include("template-parts/about/hero.php"); ?>
+<?php include("template-parts/about/grid-1.php"); ?>
+<?php include("template-parts/about/grid-2.php"); ?>
 
 <?php get_footer(); ?>
