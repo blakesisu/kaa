e1d62f0af160fd16734c01b89e86b41657b1ef29
Blake Watkins <iamsoblake@gmail.com>
2017-12-20 13:59:56 -0800

    Modify main family styles

--- site/src/template-parts/about/family.php
+++ site/src/template-parts/about/family.php
@@ -25,10 +25,11 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-grant-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-grant-lg.jpg" alt="">
         </picture>
-
         <div class="about-family-description">
-          <h3 class="about-family-name">GRANT KIRKPATRICK</h3>
-          <span class="about-family-title">Founding Partner</span>
+          <div class="about-family-info">
+            <h3 class="about-family-name">GRANT KIRKPATRICK</h3>
+            <span class="about-family-title">Founding Partner</span>
+          </div>
         </div>
       </div>
       <div class="about-family-person">
@@ -37,6 +38,12 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-duan-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-duan-lg.jpg" alt="">
         </picture>
+        <div class="about-family-description">
+          <div class="about-family-info">
+            <h3 class="about-family-name">DUAN TRAN</h3>
+            <span class="about-family-title">Partner</span>
+          </div>
+        </div>
       </div>
       <div class="about-family-person">
         <picture class="about-image-block__picture">
@@ -44,6 +51,12 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-patti-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-patti-lg.jpg" alt="">
         </picture>
+        <div class="about-family-description">
+          <div class="about-family-info">
+            <h3 class="about-family-name">PATTI BAKER</h3>
+            <span class="about-family-title">Principal</span>
+          </div>
+        </div>
       </div>
       <div class="about-family-person">
         <picture class="about-image-block__picture">
@@ -51,6 +64,12 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-steve-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-steve-lg.jpg" alt="">
         </picture>
+        <div class="about-family-description">
+          <div class="about-family-info">
+            <h3 class="about-family-name">STEVE STRAUGHAN</h3>
+            <span class="about-family-title">Design Coordinator</span>
+          </div>
+        </div>
       </div>
       <div class="about-family-person">
         <picture class="about-image-block__picture">
@@ -58,6 +77,12 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-lisa-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-lisa-lg.jpg" alt="">
         </picture>
+        <div class="about-family-description">
+          <div class="about-family-info">
+            <h3 class="about-family-name">LISA COPELAND</h3>
+            <span class="about-family-title">CFO</span>
+          </div>
+        </div>
       </div>
       <div class="about-family-person">
         <picture class="about-image-block__picture">
@@ -65,6 +90,12 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-michael-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-michael-lg.jpg" alt="">
         </picture>
+        <div class="about-family-description">
+          <div class="about-family-info">
+            <h3 class="about-family-name">MICHAEL MCGOWAN</h3>
+            <span class="about-family-title">Landscape Studio Director</span>
+          </div>
+        </div>
       </div>
       <div class="about-family-person">
         <picture class="about-image-block__picture">
@@ -72,6 +103,12 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-meghan-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-meghan-lg.jpg" alt="">
         </picture>
+        <div class="about-family-description">
+          <div class="about-family-info">
+            <h3 class="about-family-name">MEGHAN BECKMANN</h3>
+            <span class="about-family-title">Project Manager</span>
+          </div>
+        </div>
       </div>
       <div class="about-family-person">
         <picture class="about-image-block__picture">
@@ -79,6 +116,12 @@
           <source srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-todd-lg.jpg">
           <img class="about-image-block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/family/family-leader-todd-lg.jpg" alt="">
         </picture>
+        <div class="about-family-description">
+          <div class="about-family-info">
+            <h3 class="about-family-name">TODD PAOLILLO</h3>
+            <span class="about-family-title">Sr. Project Manager</span>
+          </div>
+        </div>
       </div>
 
     </div>
