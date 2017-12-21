7e364e66412b87533b4910fd4fec5883b5b2cc11
Steven Waller <jstevenwaller@gmail.com>
2017-12-20 17:31:36 -0800

    Added KAA Instagram feed to home page

--- site/src/scripts/home/home.js
+++ site/src/scripts/home/home.js
@@ -10,14 +10,14 @@ if ($('.hm-instagram').length > 0) {
   // init instagram feed
   feed = new Instafeed({
     get: 'user',
-    userId: 5780039093,
-    accessToken: '5780039093.9f4e4ba.26b3b990210345ed975fe1fedc2a4564',
+    userId: 5792963322,
+    accessToken: '5792963322.1677ed0.9da887a2336d4cc99c48250a3c96130f',
     limit: 12,
-    resolution: 'low_resolution',
+    resolution: 'standard_resolution',
     template: `
-      <li class="hm-instagram__item">
+      <li class="hm-instagram__item is-{{orientation}}">
         <a class="hm-instagram__link" href="{{link}}" target="_blank">
-          <img class="hm-instagram__img" src="{{image}}" />
+          <img class="hm-instagram__img is-{{orientation}}" src="{{image}}" />
         </a>
       </li>
     `,
