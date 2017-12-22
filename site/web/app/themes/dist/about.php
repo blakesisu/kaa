20d4c64a17aeab1472a23ab6e03060c220cac7c1
Blake Watkins <iamsoblake@gmail.com>
2017-12-21 21:38:10 -0800

    Rename more family to ext family. Fix ext family desktop padding

--- site/web/app/themes/dist/about.php
+++ site/web/app/themes/dist/about.php
@@ -11,7 +11,7 @@ get_header(); ?>
 <?php include("template-parts/about/values.php"); ?>
 <?php include("template-parts/about/vision.php"); ?>
 <?php include("template-parts/about/family.php"); ?>
-<?php include("template-parts/about/more-family.php"); ?>
+<?php include("template-parts/about/ext-family.php"); ?>
 <?php include("template-parts/about/join.php"); ?>
 <?php include("template-parts/about/contact.php"); ?>
 <?php include("template-parts/about/goodbye-hero.php"); ?>
---
+++ site/web/app/themes/dist/template-parts/about/ext-family.php
@@ -0,0 +1,68 @@
+<section class="about-inline about-ext-family">
+  <ul class="about-ext-family-members">
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">BRADLEY COOPER</h3>
+      <span class="about-ext-family-title">Sr. Project Coordinator</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">MARC DOBBS</h3>
+      <span class="about-ext-family-title">Office Manager</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">WALTER GARCIA</h3>
+      <span class="about-ext-family-title">Project Coordinator</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">PHILIP GROCHER</h3>
+      <span class="about-ext-family-title">Project Manager</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">AGNIESZKA HOPKINS</h3>
+      <span class="about-ext-family-title">Designer</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">JOYCE LOPEZ</h3>
+      <span class="about-ext-family-title">Executive Assistant</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">CECILIA MCLAREN</h3>
+      <span class="about-ext-family-title">Receptionist</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">REBECCA MONTROSS</h3>
+      <span class="about-ext-family-title">Sr. Landscape Project Coordinator</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">DAN MURPHY</h3>
+      <span class="about-ext-family-title">Sr. Project Manager</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">MARK NICHOLS</h3>
+      <span class="about-ext-family-title">Project Coordinator</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">MATTHEW PAOLA</h3>
+      <span class="about-ext-family-title">Project Manager</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">DANIEL PARENTI</h3>
+      <span class="about-ext-family-title">Landscape Project Coordinator</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">ERICK PRINS</h3>
+      <span class="about-ext-family-title">Designer</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">JASON STRAIGHT</h3>
+      <span class="about-ext-family-title">Project Coordinator</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">TRAVIS TURNER</h3>
+      <span class="about-ext-family-title">Designer</span>
+    </li>
+    <li class="about-ext-family-container">
+      <h3 class="about-ext-family-name">RYAN TYNI</h3>
+      <span class="about-ext-family-title">Project Coordinator</span>
+    </li>
+  </ul>
+</section>
