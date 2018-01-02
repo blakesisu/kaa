a888160596c2189bdbf51325ef42f22f3e154320
Blake Watkins <iamsoblake@gmail.com>
2018-01-02 14:38:52 -0800

    Fix welcome typo

--- site/web/app/themes/dist/template-parts/about/welcome.php
+++ site/web/app/themes/dist/template-parts/about/welcome.php
@@ -2,7 +2,7 @@
 
     <!-- Text Block -->
       <div class="about-text-single-blurb">
-        <span>Welcome to KAA Design. Led by out founding Partner Grant Kirkpatrick, Partner Duan Tran, and Principal Patti Baker, we are a design studio that has been dedicated to advancing the California lifestyle through contemporary architecture since 1988.</span>
+        <span>Welcome to KAA Design. Led by our founding Partner Grant Kirkpatrick, Partner Duan Tran, and Principal Patti Baker, we are a design studio that has been dedicated to advancing the California lifestyle through contemporary architecture since 1988.</span>
       </div>
 
 </section>
