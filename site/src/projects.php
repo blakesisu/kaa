d5ea8588d153ba0b4a5ec4d36cdc9136c871fcc6
Blake Watkins <iamsoblake@gmail.com>
2018-01-02 15:14:02 -0800

    Create boilerplate for projects page

---
+++ site/src/projects.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * projects page template
+ * Template Name: projects
+ * parts directory: template-parts/projects
+ */
+get_header(); ?>
+
+<?php include("template-parts/projects/section1.php"); ?>
+
+<?php get_footer(); ?>
---
+++ site/web/app/themes/dist/template-parts/projects/section1.php
@@ -0,0 +1,11 @@
+<section class="proj-hero">
+  <div class="proj-hero__container">
+    <h1 class="proj-hero__header">PROJECTS</h1>
+    <div class="proj-hero__dash"></div>
+    <picture class="proj-hero__picture">
+      <source srcset="<?php bloginfo('template_url'); ?>/images/projects/section1/projects-section1-sm.jpg" media="(max-width: 700px)">
+      <source srcset="<?php bloginfo('template_url'); ?>/images/projects/section1/projects-section1-lg.jpg">
+      <img class="proj-hero__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/section1/projects-section1-lg.jpg" alt="">
+    </picture>
+  </div>
+</section>
