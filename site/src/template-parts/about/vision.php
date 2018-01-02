2c941f61c31f4137f167ced1e99feef965607284
Blake Watkins <iamsoblake@gmail.com>
2018-01-02 14:40:07 -0800

    Fix typo in vision blurb

--- site/src/template-parts/about/vision.php
+++ site/src/template-parts/about/vision.php
@@ -2,7 +2,7 @@
 
     <!-- Text Block -->
       <div class="about-text-single-blurb">
-        <span>Our creative vision, combined with the individual talents and experience of our long-term leaders, brings a unique design perspectivee to our clients - one that seeks to elevate the daily rituals of their lives from the mundane to the sublime.</span>
+        <span>Our creative vision, combined with the individual talents and experience of our long-term leaders, brings a unique design perspective to our clients - one that seeks to elevate the daily rituals of their lives from the mundane to the sublime.</span>
       </div>
 
 </section>
