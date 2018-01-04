8f24cce75b48c0a5be5684ac5b04aee3c6dc0249
Blake Watkins <iamsoblake@gmail.com>
2018-01-04 15:59:02 -0800

    Add location titles. Adjust font and padding.
    Also add rollover effect.

--- site/src/template-parts/projects/section1.php
+++ site/src/template-parts/projects/section1.php
@@ -5,19 +5,25 @@
 
     <div class="proj__dual-projects">
       <div class="proj-projects__project proj-project__dual">
-          <picture class="proj-project__picture">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/3rdStreet-sm.jpg" media="(max-width: 700px)">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/3rdStreet-lg.jpg">
-            <img class="proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/3rdStreet-lg.jpg" alt="">
-          </picture>
+        <picture class="proj-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/3rdStreet-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/3rdStreet-lg.jpg">
+          <img class="proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/3rdStreet-lg.jpg" alt="">
+        </picture>
+        <div class="proj-project__description">
+          <h3 class="proj-text-mini__header">3RD STREET</h3>
+        </div>
       </div>
 
       <div class="proj-projects__project proj-project__dual">
-          <picture class="proj-project__picture">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/7thWalkstreet-sm.jpg" media="(max-width: 700px)">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/7thWalkstreet-lg.jpg">
-            <img class="pd-related-project__img proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/7thWalkstreet-lg.jpg" alt="">
-          </picture>
+        <picture class="proj-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/7thWalkstreet-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/7thWalkstreet-lg.jpg">
+          <img class="pd-related-project__img proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/7thWalkstreet-lg.jpg" alt="">
+        </picture>
+        <div class="proj-project__description">
+          <h3 class="proj-text-mini__header">7TH WALKSTREET</h3>
+        </div>
       </div>
 
     </div>
--- site/web/app/themes/dist/template-parts/projects/section7.php
+++ site/web/app/themes/dist/template-parts/projects/section7.php
@@ -2,19 +2,25 @@
   <div class="proj-projects__dual-container">
     <div class="proj__dual-projects">
       <div class="proj-projects__project proj-project__dual">
-          <picture class="proj-project__picture">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/31ststreet-sm.jpg" media="(max-width: 700px)">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/31ststreet-lg.jpg">
-            <img class="proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/31ststreet-lg.jpg" alt="">
-          </picture>
+        <picture class="proj-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/31ststreet-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/31ststreet-lg.jpg">
+          <img class="proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/31ststreet-lg.jpg" alt="">
+        </picture>
+        <div class="proj-project__description">
+          <h3 class="proj-text-mini__header">31ST STREET</h3>
+        </div>
       </div>
 
       <div class="proj-projects__project proj-project__dual">
-          <picture class="proj-project__picture">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/18thwalk-sm.jpg" media="(max-width: 700px)">
-            <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/18thwalk-lg.jpg">
-            <img class="pd-related-project__img proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/18thwalk-lg.jpg" alt="">
-          </picture>
+        <picture class="proj-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/18thwalk-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/18thwalk-lg.jpg">
+          <img class="pd-related-project__img proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/18thwalk-lg.jpg" alt="">
+        </picture>
+        <div class="proj-project__description">
+          <h3 class="proj-text-mini__header">18TH WALKSTREET III</h3>
+        </div>
       </div>
 
     </div>
