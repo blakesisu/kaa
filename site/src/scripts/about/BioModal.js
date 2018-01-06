5897db95ae1c301681fcc3483e04ac92bcb1884b
Blake Watkins <iamsoblake@gmail.com>
2018-01-05 17:07:41 -0800

    Bio overlay scripts

--- site/src/scripts/about/BioModal.js
+++ site/src/scripts/about/BioModal.js
@@ -1,8 +1,30 @@
 import $ from 'jquery';
-import postal from 'postal';
-import { GLOBAL_CHANNEL } from '../constants/Constants';
+// import postal from 'postal';
+// import { GLOBAL_CHANNEL } from '../constants/Constants';
 
 const BioModal = function (el) {
+  const $el = $(el);
+  const $link = $(el).find('.about-family-link');
+  const $bio = $(el).find('.about-family-bio');
+
+  $link.on('click', (e) => {
+    const targetedPopup = $bio.attr('data-popup-open');
+    console.log('open sesame see popup', targetedPopup);
+    $(`[data-popup="${targetedPopup}"]`).fadeIn(350);
+    $('.about-family').css('pointer-events', 'none');
+
+    e.preventDefault();
+  })
+
+
+  $('[data-popup-close]').on('click', function(e) {
+    console.log('close the sesame');
+    const targetedPopup = $(this).attr('data-popup-close');
+    $(`[data-popup="${targetedPopup}"]`).fadeOut(350);
+    $('.about-family').css('pointer-events', '');
+
+    e.preventDefault();
+  })
 }
 
 export default BioModal;
--- site/src/scripts/about/about.js
+++ site/src/scripts/about/about.js
@@ -1,3 +1,11 @@
 import $ from 'jquery';
-import postal from 'postal';
+// import postal from 'postal';
 import BioModal from './BioModal';
+
+if ($('body').hasClass('page-template-about')) {
+  // for each grid item create a Grid object
+  $('.about-family-person').each((i, el) => {
+    /* eslint-disable no-new */
+    new BioModal(el);
+  });
+}
