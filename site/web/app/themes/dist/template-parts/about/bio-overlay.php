c084c4ac6e78e13eaeb024eb8c0ceb40472793cc
Blake Watkins <iamsoblake@gmail.com>
2018-01-08 16:50:01 -0800

    Restructure bio overlay for mobile reordering

--- site/web/app/themes/dist/template-parts/about/bio-overlay.php
+++ site/web/app/themes/dist/template-parts/about/bio-overlay.php
@@ -1,20 +1,24 @@
 <section class="popup about-bio-overlay" data-popup="kirkpatrick">
   <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal gkirkpatrick background-img background-left">
+    <div class="about-bio-overlay__left gkirkpatrick background-img background-left">
       <!-- <picture class="about&#45;image&#45;block__picture"> -->
       <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/GKirkpatrick.JPG" media="(max&#45;width: 700px)"> -->
       <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/GKirkpatrick.JPG"> -->
       <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/GKirkpatrick.JPG" alt=""> -->
       <!-- </picture> -->
     </div>
-    <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">GRANT KIRKPATRICK</h2>
-        <h3 class="about-join-title">FOUNDING PARTNER</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
-        </h2>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">GRANT KIRKPATRICK</h2>
+      <h3 class="about-bio-title">FOUNDING PARTNER</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
+    <div class="about-bio-overlay__right">
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="kirkpatrick" href="#">x</a>
@@ -22,29 +26,41 @@
 </section>
 
 <section class="popup about-bio-overlay" data-popup="tran">
-  <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal dtran background-img background-left">
-      <!-- <picture class="about&#45;image&#45;block__picture"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/DTran.JPG" media="(max&#45;width: 700px)"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/DTran.JPG"> -->
-      <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/DTran.JPG" alt=""> -->
-      <!-- </picture> -->
+  <div class="popup-inner">
+    <div class="about-bio-overlay__left dtran background-img background-left">
     </div>
-    <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">DUAN TRAN</h2>
-        <h3 class="about-join-title">PARTNER</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">A native Ohioan, Duan Tran earned his bachelor's degree from the University of Miami Ohio and master's degree from the University of Southern California. He has worked for several distinguished firms in both Ohio and Washington DC prior to joining KAA in 2000. Since then, Duan has overseen several high profile custom residential projects for many notable clients. His projects have been recognized for design by the AIA, as well as have been featured in several national shelter publications, including <em>Architechtural Digest</em>.
-<br>
-<br>
-Drawn to and fundamentally inspired by Southern California's vibrant lifestyle and culture, Duan's design approach seeks to infuse this influence with an inherent optismism through fun, warm, and contextually relevant design.
-<br>
-<br>
-Duan is a member of the American Institute of Architects (AIA) and is a licensed Architect in the states of Nevada and Hawaii. He is certified by the national Council of Architectural Registration Boards (NCARB). He is also a member fo the University of Southern California's Architechtural Guild and the Los Angeles Conservancy.
-</p>
-        </h2>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">DUAN TRAN</h2>
+      <h3 class="about-bio-title">PARTNER</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
+    <div class="about-bio-overlay__right">
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">A native Ohioan, Duan Tran earned his bachelor's degree from the University of Miami Ohio and master's degree from the University of Southern California. He has worked for several distinguished firms in both Ohio and Washington DC prior to joining KAA in 2000. Since then, Duan has overseen several high profile custom residential projects for many notable clients. His projects have been recognized for design by the AIA, as well as have been featured in several national shelter publications, including <em>Architechtural Digest</em>.
+          <br>
+          <br>
+  Drawn to and fundamentally inspired by Southern California's vibrant lifestyle and culture, Duan's design approach seeks to infuse this influence with an inherent optismism through fun, warm, and contextually relevant design.
+          <br>
+          <br>
+  Duan is a member of the American Institute of Architects (AIA) and is a licensed Architect in the states of Nevada and Hawaii. He is certified by the national Council of Architectural Registration Boards (NCARB). He is also a member fo the University of Southern California's Architechtural Guild and the Los Angeles Conservancy.
+          </p>
+          <br>
+          <br>
+          <div class="about-bio-articles">
+            <div class="about-bio-articles-header-container">
+              <h2 class="about-bio-articles-header">CONTRIBUTED ARTICLES</h2>
+            </div>
+            <article class="about-bio-article">
+              <h3 class="about-bio-article-title">Historic Preservation Today ></h3>
+              <p class="about-bio-bio">
+                Ken Breisch, former director of USC's Graduate Program in Historic Preservation, speaks on the history and philosophy of the preservation movement as it has evolved during the past century and its relevance to Southern California's treasures.
+              </p>
+            </article>
+          </div>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="tran" href="#">x</a>
@@ -53,21 +69,20 @@ Duan is a member of the American Institute of Architects (AIA) and is a licensed
 
 <section class="popup about-bio-overlay" data-popup="baker">
   <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal pbaker background-img background-left">
-      <!-- <picture class="about&#45;image&#45;block__picture"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/PBaker.JPG" media="(max&#45;width: 700px)"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/PBaker.JPG"> -->
-      <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/PBaker.JPG" alt=""> -->
-      <!-- </picture> -->
+    <div class="about-bio-overlay__left pbaker background-img background-left">
     </div>
-    <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">PATTI BAKER</h2>
-        <h3 class="about-join-title">PRINCIPAL</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
-        </h2>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">PATTI BAKER</h2>
+      <h3 class="about-bio-title">PRINCIPAL</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
+    <div class="about-bio-overlay__right">
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="baker" href="#">x</a>
@@ -76,21 +91,20 @@ Duan is a member of the American Institute of Architects (AIA) and is a licensed
 
 <section class="popup about-bio-overlay" data-popup="straughan">
   <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal sstraughan background-img background-left">
-      <!-- <picture class="about&#45;image&#45;block__picture"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/SStraughan.JPG" media="(max&#45;width: 700px)"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/SStraughan.JPG"> -->
-      <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/SStraughan.JPG" alt=""> -->
-      <!-- </picture> -->
+    <div class="about-bio-overlay__left sstraughan background-img background-left">
     </div>
-    <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">STEVE STRAUGHAN</h2>
-        <h3 class="about-join-title">DESIGN COORDINATOR</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
-        </h2>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">STEVE STRAUGHAN</h2>
+      <h3 class="about-bio-title">DESIGN COORDINATOR</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
+    <div class="about-bio-overlay__right">
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="straughan" href="#">x</a>
@@ -99,21 +113,20 @@ Duan is a member of the American Institute of Architects (AIA) and is a licensed
 
 <section class="popup about-bio-overlay" data-popup="copeland">
   <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal lcopeland background-img background-right">
-      <!-- <picture class="about&#45;image&#45;block__picture"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/LCopeland.JPG" media="(max&#45;width: 700px)"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/LCopeland.JPG"> -->
-      <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/LCopeland.JPG" alt=""> -->
-      <!-- </picture> -->
+    <div class="about-bio-overlay__left lcopeland background-img background-right">
     </div>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">LISA COPELAND</h2>
+      <h3 class="about-bio-title">CFO</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
     <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">LISA COPELAND</h2>
-        <h3 class="about-join-title">CFO</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
-        </h2>
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="copeland" href="#">x</a>
@@ -122,21 +135,20 @@ Duan is a member of the American Institute of Architects (AIA) and is a licensed
 
 <section class="popup about-bio-overlay" data-popup="mcgowan">
   <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal mmcgowan background-img background-right">
-      <!-- <picture class="about&#45;image&#45;block__picture"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/MMcgowan.JPG" media="(max&#45;width: 700px)"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/MMcgowan.JPG"> -->
-      <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/MMcgowan.JPG" alt=""> -->
-      <!-- </picture> -->
+    <div class="about-bio-overlay__left mmcgowan background-img background-right">
     </div>
-    <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">MICHAEL MCGOWAN</h2>
-        <h3 class="about-join-title">LANDSCAPE STUDIO DIRECTOR</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
-        </h2>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">MICHAEL MCGOWAN</h2>
+      <h3 class="about-bio-title">LANDSCAPE STUDIO DIRECTOR</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
+    <div class="about-bio-overlay__right">
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="mcgowan" href="#">x</a>
@@ -145,21 +157,20 @@ Duan is a member of the American Institute of Architects (AIA) and is a licensed
 
 <section class="popup about-bio-overlay" data-popup="beckmann">
   <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal mbeckmann background-img background-left">
-      <!-- <picture class="about&#45;image&#45;block__picture"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/MBeckmann.tif" media="(max&#45;width: 700px)"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/MBeckmann.tif"> -->
-      <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/MBeckmann.tif" alt=""> -->
-      <!-- </picture> -->
+    <div class="about-bio-overlay__left mbeckmann background-img background-left">
     </div>
-    <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">MEGHAN BECKMANN</h2>
-        <h3 class="about-join-title">PROJECT MANAGER</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
-        </h2>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">MEGHAN BECKMANN</h2>
+      <h3 class="about-bio-title">PROJECT MANAGER</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
+    <div class="about-bio-overlay__right">
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="beckmann" href="#">x</a>
@@ -168,21 +179,20 @@ Duan is a member of the American Institute of Architects (AIA) and is a licensed
 
 <section class="popup about-bio-overlay" data-popup="paolillo">
   <div class="popup-inner about-inline">
-    <div class="about-bio-overlay__left about-horizontal tpaolillo background-img background-right">
-      <!-- <picture class="about&#45;image&#45;block__picture"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/TPaolillo.JPG" media="(max&#45;width: 700px)"> -->
-      <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/about/bio/TPaolillo.JPG"> -->
-      <!--   <img class="about&#45;image&#45;block__img" srcset="<?php bloginfo('template_url'); ?>/images/about/bio/TPaolillo.JPG" alt=""> -->
-      <!-- </picture> -->
+    <div class="about-bio-overlay__left tpaolillo background-img background-right">
     </div>
-    <div class="about-bio-overlay__right about-horizontal">
-      <div class="about-value-text-column">
-        <h2 class="about-value-header">TODD PAOLILLO</h2>
-        <h3 class="about-join-title">SR. PROJECT MANAGER</h3>
-        <h2>
-          <div class="about-text-block__dash"></div>
-          <p class="about-value-content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
-        </h2>
+
+    <div class="about-bio-header-section">
+      <h2 class="about-bio-header">TODD PAOLILLO</h2>
+      <h3 class="about-bio-title">SR. PROJECT MANAGER</h3>
+      <div class="about-bio-dash"></div>
+    </div>
+
+    <div class="about-bio-overlay__right">
+      <div class="about-bio-text-column">
+        <div class="about-bio-content">
+          <p class="about-bio-bio">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quesque volutpat mattis eros. Nullam malesuada era ut turpis. Suspendisse urna nibh, viverra non, semper suscipit.</p>
+        </div>
       </div>
     </div>
     <a class="popup-close" data-popup-close="paolillo" href="#">x</a>
