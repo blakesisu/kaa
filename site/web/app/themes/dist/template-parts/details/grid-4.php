9b6a47a4ad3d96397a115f07b840f59144ba8e5c
Blake Watkins <iamsoblake@gmail.com>
2018-01-11 14:32:48 -0800

    stop tracking dist

--- site/web/app/themes/dist/template-parts/details/grid-4.php
+++
@@ -1,9 +0,0 @@
-<section class="pd-grid pd-grid--full">
-  <div class="pd-grid__block pd-grid__block--image">
-    <picture class="pd-grid__picture">
-      <source srcset="<?php bloginfo('template_url'); ?>/images/details/grid-4/details-poolside-sm.jpg" media="(max-width: 700px)">
-      <source srcset="<?php bloginfo('template_url'); ?>/images/details/grid-4/details-poolside-lg.jpg">
-      <img class="pd-grid__img" srcset="<?php bloginfo('template_url'); ?>/images/details/grid-4/details-poolside-lg.jpg" alt="">
-    </picture>
-  </div>
-</section>
