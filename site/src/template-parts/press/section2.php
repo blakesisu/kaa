0fd665a1ca1f82e9ac431e223c5f3ae079457652
Blake Watkins <iamsoblake@gmail.com>
2018-01-22 14:52:29 -0800

    Add images

--- site/src/template-parts/press/section2.php
+++ site/src/template-parts/press/section2.php
@@ -1,39 +1,43 @@
 <section class="press-section2">
-  <div class="press-projects__tri-container">
+  <div class="press-trip-container">
+    <a href="#" class="press-trip-item">
+      <picture class="press-picture-section2">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/margolis-sm.png" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/margolis
+.jpg">
+        <img class="press-img-section2" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/margolis.jpg" alt="margolis">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+    </a>
 
-    <div class="press__tri-projects">
-      <a href="#" class="press-projects__project press-project__tri">
-        <picture class="press-project__picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/towergrovedrive-sm.jpg" media="(max-width: 700px)">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/towergrovedrive-lg.jpg">
-          <img class="press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/towergrovedrive-lg.jpg" alt="">
-        </picture>
-        <div class="press-project__description">
-          <h3 class="press-text-mini__header">TOWER GROVE DRIVE</h3>
-        </div>
-      </a>
+    <a href="#" class="press-trip-item">
+      <picture class="press-picture-section2">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/million-deco-sm.png" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/million-deco.jpg">
+        <img class="press-img-section2" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/million-deco.jpg" alt="deco">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+    </a>
 
-      <a href="#" class="press-projects__project press-project__tri">
-        <picture class="press-project__picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/strand-sm.jpg" media="(max-width: 700px)">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/strand-lg.jpg">
-          <img class="pd-related-project__img press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/strand-lg.jpg" alt="">
-        </picture>
-        <div class="press-project__description">
-          <h3 class="press-text-mini__header">3RD STREET &<br>THE STRAND RESIDENCE</h3>
-        </div>
-      </a>
-
-      <a href="#" class="press-projects__project press-project__tri">
-        <picture class="press-project__picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/balancehill-sm.jpg" media="(max-width: 700px)">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/balancehill-lg.jpg">
-          <img class="pd-related-project__img press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/balancehill-lg.jpg" alt="">
-        </picture>
-        <div class="press-project__description">
-          <h3 class="press-text-mini__header">BALANCE HILL</h3>
-        </div>
-      </a>
-    </div>
+    <a href="#" class="press-trip-item">
+      <picture class="press-picture-section2">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/milk-sm.png" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/milk.jpg">
+        <img class="press-img-section2" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/milk.jpg" alt="milk">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+    </a>
   </div>
 </section>
