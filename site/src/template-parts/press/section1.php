a04457179c41d92acde21a8d45d2fa8fb1b7fdee
Blake Watkins <iamsoblake@gmail.com>
2018-01-22 13:39:33 -0800

    Work on feature section

--- site/src/template-parts/press/section1.php
+++ site/src/template-parts/press/section1.php
@@ -1,22 +1,23 @@
 <section class="press-section1">
-  <div class="about-value">
+  <div class="press-flex-container">
     <!-- Image Block -->
-    <div class="about-value__img-column">
-      <div class="about-value__img-wrapper">
-        <picture class="about-value__picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/Book.png" media="(max-width: 700px)">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg">
-          <img class="press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg" alt="">
-        </picture>
+    <div class="press-column-item">
+      <div class="press-img-wrapper">
+        <!-- <picture class="press&#45;picture"> -->
+        <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/Book.png" media="(max&#45;width: 700px)"> -->
+        <!--   <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg"> -->
+        <!--   <img class="press&#45;book&#45;img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg" alt=""> -->
+        <!-- </picture> -->
       </div>
     </div>
 
     <!-- Text Block TODO: check landing for layout inspiration on text blocks-->
-    <div class="about-value__text-column">
-      <div class="about-value__text-wrapper">
-        <h2 class="about-value__header">ARCHITECTURAL INTEGRITY &amp; AUTHENTICITY</h2>
-        <div class="about-value__dash"></div>
-        <p class="about-value__description">Morbi in sem quis dui placerat ornare, pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu cras neque id cursus faucibus consequat.</p>
+    <div class="press-column-item">
+      <div class="press-text-wrapper press-text-section1">
+        <h2 class="press-text-header">CALIFORNIA CONTEMPORARY:</h2>
+        <h2 class="press-text-header">A Book Over 30 Years in the Making</h2>
+        <p class="press-text-description">Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+        <button class="press-read-button">READ MORE</button>
       </div>
     </div>
   </div>
