7c76d33a1c581603f74acb5404d20aef5f16b35c
Blake Watkins <iamsoblake@gmail.com>
2018-01-22 11:53:20 -0800

    Create press boilerplate

---
+++ site/src/press.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * press page template
+ * Template Name: press
+ * parts directory: template-parts/press
+ */
+get_header(); ?>
+
+<?php include("template-parts/press/section1.php"); ?>
+<?php include("template-parts/press/section2.php"); ?>
+
+<?php get_footer(); ?>
---
+++ site/src/template-parts/press/section2.php
@@ -0,0 +1,39 @@
+<section class="press-section2">
+  <div class="press-projects__tri-container">
+
+    <div class="press__tri-projects">
+      <a href="#" class="press-projects__project press-project__tri">
+        <picture class="press-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/towergrovedrive-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/towergrovedrive-lg.jpg">
+          <img class="press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/towergrovedrive-lg.jpg" alt="">
+        </picture>
+        <div class="press-project__description">
+          <h3 class="press-text-mini__header">TOWER GROVE DRIVE</h3>
+        </div>
+      </a>
+
+      <a href="#" class="press-projects__project press-project__tri">
+        <picture class="press-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/strand-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/strand-lg.jpg">
+          <img class="pd-related-project__img press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/strand-lg.jpg" alt="">
+        </picture>
+        <div class="press-project__description">
+          <h3 class="press-text-mini__header">3RD STREET &<br>THE STRAND RESIDENCE</h3>
+        </div>
+      </a>
+
+      <a href="#" class="press-projects__project press-project__tri">
+        <picture class="press-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/balancehill-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/balancehill-lg.jpg">
+          <img class="pd-related-project__img press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/balancehill-lg.jpg" alt="">
+        </picture>
+        <div class="press-project__description">
+          <h3 class="press-text-mini__header">BALANCE HILL</h3>
+        </div>
+      </a>
+    </div>
+  </div>
+</section>
