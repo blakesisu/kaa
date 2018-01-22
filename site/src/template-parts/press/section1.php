5bce895c7334baf380e51d7f11b77879ebaec3b2
Blake Watkins <iamsoblake@gmail.com>
2018-01-22 11:53:20 -0800

    Create press boilerplate

---
+++ site/src/template-parts/press/section1.php
@@ -0,0 +1,23 @@
+<section class="press-section1">
+  <div class="about-value">
+    <!-- Image Block -->
+    <div class="about-value__img-column">
+      <div class="about-value__img-wrapper">
+        <picture class="about-value__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/Book.png" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg">
+          <img class="press-project__img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg" alt="">
+        </picture>
+      </div>
+    </div>
+
+    <!-- Text Block TODO: check landing for layout inspiration on text blocks-->
+    <div class="about-value__text-column">
+      <div class="about-value__text-wrapper">
+        <h2 class="about-value__header">ARCHITECTURAL INTEGRITY &amp; AUTHENTICITY</h2>
+        <div class="about-value__dash"></div>
+        <p class="about-value__description">Morbi in sem quis dui placerat ornare, pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu cras neque id cursus faucibus consequat.</p>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ site/src/template-parts/press/section3.php
@@ -0,0 +1,28 @@
+<section class="proj-section3">
+  <div class="proj-projects__dual-container">
+    <div class="proj__dual-projects">
+      <a href="#" class="proj-projects__project proj-project__dual">
+        <picture class="proj-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/longfellow-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/longfellow-lg.jpg">
+          <img class="proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/longfellow-lg.jpg" alt="">
+        </picture>
+        <div class="proj-project__description">
+          <h3 class="proj-text-mini__header">LONGFELLOW DRIVE</h3>
+        </div>
+      </a>
+
+      <a href="#" class="proj-projects__project proj-project__dual">
+        <picture class="proj-project__picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/mobile/balboa-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/balancehill-lg.jpg">
+          <img class="pd-related-project__img proj-project__img" srcset="<?php bloginfo('template_url'); ?>/images/projects/desktop/balboa-lg.jpg" alt="">
+        </picture>
+        <div class="proj-project__description">
+          <h3 class="proj-text-mini__header">BALBOA AVENUE</h3>
+        </div>
+      </a>
+
+    </div>
+  </div>
+</section>
