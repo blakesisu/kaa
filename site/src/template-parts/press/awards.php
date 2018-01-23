e08250c89bf976746d20d47a4f866465943a7c2d
Blake Watkins <iamsoblake@gmail.com>
2018-01-22 18:07:27 -0800

    Finish overall rough scaffold of page

--- site/src/template-parts/press/awards.php
+++ site/src/template-parts/press/awards.php
@@ -1,2 +1,134 @@
-<section class="press-section press-awards">
+<section class="press-section about-grid press-awards">
+  <div class="press-header-container">
+    <h1 class="press-awards-header">AWARDS</h1>
+  </div>
+
+  <ul class="press-awards-members">
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+    <li class="press-awards-container">
+      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
+      <span class="press-awards-title">Citation Award for Excellence in Design /
+      <br>
+        <span>Balance Hill</span>
+      </span>
+    </li>
+  </ul>
 </section>
--- site/src/template-parts/press/media.php
+++ site/src/template-parts/press/media.php
@@ -1,2 +1,124 @@
 <section class="press-section press-media">
+  <div class="press-media-header">
+    <div class="press-media-headline">
+      <h1>KAA DESIGN IN THE MEDIA</h1>
+    </div>
+    <div class="press-media-dropdown">
+      <p>ALL PUBLICATIONS</p>
+    </div>
+    <div class="press-media-nav-arrows">
+      <!-- TODO: obtain correct chevrons from psd -->
+      <a href="#" class="press-media-next">
+          <?php include(__DIR__ ."/../svgs/chevron-right.svg"); ?>
+      </a>
+      <a href="#" class="press-media-back">
+          <?php include(__DIR__ ."/../svgs/chevron-right.svg"); ?>
+      </a>
+    </div>
+  </div>
+
+  <div class="press-media-mags">
+    <div class="press-media-mag-item">
+      <picture class="press-picture">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/digs-mag.jpg" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/digs-mag.jpg">
+        <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/digs-mag.jpg" alt="exhibit">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+      <div class="press-news-actions">
+        <div class="press-media-pdf">PDF</div>
+        <div class="press-media-read">READ</div>
+      </div>
+    </div>
+
+    <div class="press-media-mag-item">
+      <picture class="press-picture">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/living-mag.jpg" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/living-mag.jpg">
+        <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/exhibit.jpg" alt="exhibit">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+      <div class="press-news-actions">
+        <div class="press-media-pdf">PDF</div>
+        <div class="press-media-read">READ</div>
+      </div>
+    </div>
+
+    <div class="press-media-mag-item">
+      <picture class="press-picture">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/coastal-mag.jpg" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/coastal-mag.jpg">
+        <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/coastal-mag.jpg" alt="exhibit">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+      <div class="press-news-actions">
+        <div class="press-media-pdf">PDF</div>
+        <div class="press-media-read">READ</div>
+      </div>
+    </div>
+
+    <div class="press-media-mag-item">
+      <picture class="press-picture">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/ocean-mag.jpg" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/ocean-mag.jpg">
+        <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/ocean-mag.jpg" alt="exhibit">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+      <div class="press-news-actions">
+        <div class="press-media-pdf">PDF</div>
+        <div class="press-media-read">READ</div>
+      </div>
+    </div>
+
+    <div class="press-media-mag-item">
+      <picture class="press-picture">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/luxe-mag.jpg" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/luxe-mag.jpg">
+        <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/luxe-mag.jpg" alt="exhibit">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+      <div class="press-news-actions">
+        <div class="press-media-pdf">PDF</div>
+        <div class="press-media-read">READ</div>
+      </div>
+    </div>
+
+    <div class="press-media-mag-item">
+      <picture class="press-picture">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/sunset-mag.jpg" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/sunset-mag.jpg">
+        <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/sunset-mag.jpg" alt="exhibit">
+      </picture>
+      <div class="press-article-description">
+        <h3 class="press-article-date">Dec 4, 2017</h3>
+        <h3 class="press-article-title">Ipsum Stuff</h3>
+        <p class="press-article-content">About that ipsum stuff...</p>
+      </div>
+      <div class="press-news-actions">
+        <div class="press-media-pdf">PDF</div>
+        <div class="press-media-read">READ</div>
+      </div>
+    </div>
+
+  </div>
 </section>
