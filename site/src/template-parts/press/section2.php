75c5ab2f307de6e3808c3a4de56b850a60106cb8
Blake Watkins <iamsoblake@gmail.com>
2018-01-22 16:18:11 -0800

    Fix path to images and cleanup css for image sizes

--- site/src/template-parts/press/section2.php
+++ site/src/template-parts/press/section2.php
@@ -2,9 +2,8 @@
   <div class="press-trip-container">
     <a href="#" class="press-trip-item">
       <picture class="press-picture-section2">
-        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/margolis-sm.png" media="(max-width: 700px)">
-        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/margolis
-.jpg">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/margolis-sm.jpg" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/margolis.jpg">
         <img class="press-img-section2" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/margolis.jpg" alt="margolis">
       </picture>
       <div class="press-article-description">
@@ -16,7 +15,7 @@
 
     <a href="#" class="press-trip-item">
       <picture class="press-picture-section2">
-        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/million-deco-sm.png" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/million-deco-sm.jpg" media="(max-width: 700px)">
         <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/million-deco.jpg">
         <img class="press-img-section2" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/million-deco.jpg" alt="deco">
       </picture>
@@ -29,7 +28,7 @@
 
     <a href="#" class="press-trip-item">
       <picture class="press-picture-section2">
-        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/milk-sm.png" media="(max-width: 700px)">
+        <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/milk-sm.jpg" media="(max-width: 700px)">
         <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/milk.jpg">
         <img class="press-img-section2" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/milk.jpg" alt="milk">
       </picture>
