9ba639ab305cea51a5fa121b26d7bfcc9f350cd9
Blake Watkins <iamsoblake@gmail.com>
2018-01-22 18:07:27 -0800

    Finish overall rough scaffold of page

--- site/src/press.php
+++ site/src/press.php
@@ -8,5 +8,7 @@ get_header(); ?>
 
 <?php include("template-parts/press/feature.php"); ?>
 <?php include("template-parts/press/news.php"); ?>
+<?php include("template-parts/press/media.php"); ?>
+<?php include("template-parts/press/awards.php"); ?>
 
 <?php get_footer(); ?>
--- site/src/template-parts/press/news.php
+++ site/src/template-parts/press/news.php
@@ -117,4 +117,21 @@
       </div>
     </a>
   </div>
+
+  <div class="press-news-paginator">
+    <a href="#"><h2>1</h2></a>
+    <a href="#"><h2>2</h2></a>
+    <a href="#"><h2>3</h2></a>
+    <a href="#"><h2>4</h2></a>
+    <a href="#"><h2>5</h2></a>
+    <a href="#"><h2>6</h2></a>
+    <a href="#"><h2>7</h2></a>
+    <a href="#"><h2>8</h2></a>
+    <a href="#"><h2>9</h2></a>
+    <a href="#"><h2>10...</h2></a>
+
+    <a href="#" class="press-news-next">
+        <?php include(__DIR__ ."/../svgs/chevron-right.svg"); ?>
+    </a>
+  </div>
 </section>
