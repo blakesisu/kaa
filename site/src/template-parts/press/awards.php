9402085140f9422932f474c2485c86399189e6e1
Blake Watkins <iamsoblake@gmail.com>
2018-01-24 11:58:01 -0800

    Clean up desktop styles

--- site/src/template-parts/press/awards.php
+++ site/src/template-parts/press/awards.php
@@ -5,130 +5,152 @@
 
   <ul class="press-awards-members">
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | SOUTH BAY DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Citation Award for<br>Excellence in Design /<br>
+        <span>Balance Hill</span></p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">COMMERCIAL REAL ESTATE AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Best Multi-Family Project<br>Los Angeles Business Journal</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">COMMERCIAL REAL ESTATE AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Nominee<br>Los Angeles Business Journal</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | LA DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Citation Award /<br>LACMA Plaza Phase 2</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">ANNUAL RETAIL DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+      <p class="press-awards-title">NASFM Award <br>Association for Retail <br>Environments /<br>Geary's Beverly Hills</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | SOUTH BAY DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Honor Award for<br>Excellence in Design /<br> <span>Monaco Gym & Spa</span></p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | SOUTH BAY DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Merit Award for<br>Excellence in Design /<br>
+        <span>6th Street</span></p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">PATTERN BOOK COMPETITION</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Winner: Mid-Century Modern<br>ICA&CA Southern California<br>Habitat for Humanity Los Angeles</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | LA DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Merit Award /<br>LACMA Plaza</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">DESIGN COMPETITION</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">First Place Award & Commission /<br> Good Shepherd Women's Village</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AZ AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Finalist, Lighting Design<br>Azure Magazine /<br>Thuma Lantern</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | SOUTH BAY DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Citation Award for<br>Excellence in Design /<br><span>3rd Street</span></p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">15TH ANNUAL STARS OF DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">2009 Honoree: Architecture<br>Pacific Design Center</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">BEAUTIFICATION AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Los Angeles Business Council /<br>Geary's Beverly Hills</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">GOOD DESIGN IS GOOD BUSINESS</h3>
+      </div>
+      <div class="press-awards-container-header">
+        <p class="press-awards-title">Architectural Record</p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | SOUTH BAY DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Citation Award for<br>Excellence in Design</p>
+      <div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | SOUTH BAY DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">Citation Award for<br>Excellence in Design /<br>
+        Hermosa Beach Cottage<br>
+        <span>Monterey Boulevard</span></p>
+      </div>
     </li>
     <li class="press-awards-container">
-      <h3 class="press-awards-name">AIA | SOUTH BAY</h3>
-      <span class="press-awards-title">Citation Award for Excellence in Design /
-      <br>
-        <span>Balance Hill</span>
-      </span>
+      <div class="press-awards-container-header">
+        <h3 class="press-awards-name">AIA | LA DESIGN AWARDS</h3>
+      </div>
+      <div class="press-awards-content">
+        <p class="press-awards-title">/ Good Shepherd Women's Village</p>
+      </div>
     </li>
   </ul>
 </section>
