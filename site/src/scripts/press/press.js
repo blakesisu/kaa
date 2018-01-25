fa4eaac7f3c08e69908204f6bddd8fedadbeb6d8
Blake Watkins <iamsoblake@gmail.com>
2018-01-25 14:49:33 -0800

    Ugly working version of slide function in press

--- site/src/scripts/press/press.js
+++ site/src/scripts/press/press.js
@@ -1,10 +1,14 @@
 import $ from 'jquery';
+import Carousel from './Carousel';
 
 
 if ($('body').hasClass('page-template-press')) {
-  // Create leader objects
+  // Enable dropdown
   $('.press-media-dropdown').on('click', function(ev) {
     $('.press-media-dropdown-container').toggle();
   });
+
+  // Create carousel
+  const magCarousel = new Carousel('.press-media-mags');
 }
 
