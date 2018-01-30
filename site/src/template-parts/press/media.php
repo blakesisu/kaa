77e23651b8784d5d564dd0fa4a26e7d11988bb75
Blake Watkins <iamsoblake@gmail.com>
2018-01-30 11:52:03 -0800

    Fix magazine alignment issues

--- site/src/template-parts/press/media.php
+++ site/src/template-parts/press/media.php
@@ -42,97 +42,110 @@
   </div>
 
   <div class="press-media-mags swiper-container">
-    <div class="press-media-mags-wrapper swiper-wrapper">
-      <div class="press-media-mag-item swiper-slide">
-        <picture class="press-picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/digs-mag.jpg">
-          <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/digs-mag.jpg" alt="exhibit">
-        </picture>
-        <div class="press-media-article">
-          <p class="press-media-article-date">DEC 2017</p>
-          <h3 class="press-media-article-title">South Bay DIGS</h3>
-          <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Tower Grove Drive</a></p>
-        </div>
-        <div class="press-media-actions">
-          <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
-        </div>
-      </div>
+    <ul class="press-media-mags-wrapper swiper-wrapper">
 
-      <div class="press-media-mag-item swiper-slide">
-        <picture class="press-picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/living-mag.jpg">
-          <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/exhibit.jpg" alt="exhibit">
-        </picture>
-        <div class="press-media-article">
-          <p class="press-media-article-date">DEC 2017</p>
-          <h3 class="press-media-article-title">LIVING | Brazil</h3>
-          <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Tower Grove Drive</a></p>
+      <li class="press-media-mag-item swiper-slide">
+        <div class="press-media-mag-content">
+          <picture class="press-picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/digs-mag.jpg">
+            <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/digs-mag.jpg" alt="exhibit">
+          </picture>
+          <div class="press-media-article">
+            <p class="press-media-article-date">DEC 2017</p>
+            <h3 class="press-media-article-title">South Bay DIGS</h3>
+            <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Tower Grove Drive</a></p>
+          </div>
+          <div class="press-media-actions">
+            <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
+          </div>
         </div>
-        <div class="press-media-actions">
-          <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
-        </div>
-      </div>
+      </li>
 
-      <div class="press-media-mag-item swiper-slide">
-        <picture class="press-picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/coastal-mag.jpg">
-          <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/coastal-mag.jpg" alt="exhibit">
-        </picture>
-        <div class="press-media-article">
-          <p class="press-media-article-date">OCT 2017</p>
-          <h3 class="press-media-article-title">Coastal Living</h3>
+      <li class="press-media-mag-item swiper-slide">
+        <div class="press-media-mag-content">
+          <picture class="press-picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/living-mag.jpg">
+            <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/exhibit.jpg" alt="exhibit">
+          </picture>
+          <div class="press-media-article">
+            <p class="press-media-article-date">DEC 2017</p>
+            <h3 class="press-media-article-title">LIVING | Brazil</h3>
+            <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Tower Grove Drive</a></p>
+          </div>
+          <div class="press-media-actions">
+            <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
+          </div>
         </div>
-        <div class="press-media-actions">
-          <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
-        </div>
-      </div>
+      </li>
 
-      <div class="press-media-mag-item swiper-slide">
-        <picture class="press-picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/ocean-mag.jpg">
-          <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/ocean-mag.jpg" alt="exhibit">
-        </picture>
-        <div class="press-media-article">
-          <p class="press-media-article-date">AUG/SEP 2017</p>
-          <h3 class="press-media-article-title">Ocean Home</h3>
-          <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">3rd Street & The Strand Residence</a></p>
+      <li class="press-media-mag-item swiper-slide">
+        <div class="press-media-mag-content">
+          <picture class="press-picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/coastal-mag.jpg">
+            <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/coastal-mag.jpg" alt="exhibit">
+          </picture>
+          <div class="press-media-article">
+            <p class="press-media-article-date">OCT 2017</p>
+            <h3 class="press-media-article-title">Coastal Living</h3>
+          </div>
+          <div class="press-media-actions">
+            <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
+          </div>
         </div>
-        <div class="press-media-actions">
-          <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
-          <a href="#" class="press-media-read"><span>READ</span></a>
-        </div>
-      </div>
+      </li>
 
-      <div class="press-media-mag-item swiper-slide">
-        <picture class="press-picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/luxe-mag.jpg">
-          <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/luxe-mag.jpg" alt="exhibit">
-        </picture>
-        <div class="press-media-article">
-          <p class="press-media-article-date">MAY 2017</p>
-          <h3 class="press-media-article-title">LUXE Interiors + Design</h3>
-          <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Tower Grove Drive</a></p>
+      <li class="press-media-mag-item swiper-slide">
+        <div class="press-media-mag-content">
+          <picture class="press-picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/ocean-mag.jpg">
+            <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/ocean-mag.jpg" alt="exhibit">
+          </picture>
+          <div class="press-media-article">
+            <p class="press-media-article-date">AUG/SEP 2017</p>
+            <h3 class="press-media-article-title">Ocean Home</h3>
+            <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">3rd Street & The Strand Residence</a></p>
+          </div>
+          <div class="press-media-actions">
+            <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
+            <a href="#" class="press-media-read"><span>READ</span></a>
+          </div>
         </div>
-        <div class="press-media-actions">
-          <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
-        </div>
-      </div>
+      </li>
 
-      <div class="press-media-mag-item swiper-slide">
-        <picture class="press-picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/sunset-mag.jpg">
-          <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/sunset-mag.jpg" alt="exhibit">
-        </picture>
-        <div class="press-media-article">
-          <p class="press-media-article-date">MAR 2017</p>
-          <h3 class="press-media-article-title">Sunset</h3>
-          <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Split Rock Ranch</a></p>
+      <li class="press-media-mag-item swiper-slide">
+        <div class="press-media-mag-content">
+          <picture class="press-picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/luxe-mag.jpg">
+            <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/luxe-mag.jpg" alt="exhibit">
+          </picture>
+          <div class="press-media-article">
+            <p class="press-media-article-date">MAY 2017</p>
+            <h3 class="press-media-article-title">LUXE Interiors + Design</h3>
+            <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Tower Grove Drive</a></p>
+          </div>
+          <div class="press-media-actions">
+            <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
+          </div>
         </div>
-        <div class="press-media-actions">
-          <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
+      </li>
+
+      <li class="press-media-mag-item swiper-slide">
+        <div class="press-media-mag-content">
+          <picture class="press-picture">
+            <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/sunset-mag.jpg">
+            <img class="press-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/sunset-mag.jpg" alt="exhibit">
+          </picture>
+          <div class="press-media-article">
+            <p class="press-media-article-date">MAR 2017</p>
+            <h3 class="press-media-article-title">Sunset</h3>
+            <p class="press-media-article-content">Featuring <a href="#" class="press-highlight">Split Rock Ranch</a></p>
+          </div>
+          <div class="press-media-actions">
+            <a href="#" class="press-media-pdf"><?php include(__DIR__ ."/../svgs/down-chev.svg"); ?> <span>PDF</span></a>
+          </div>
         </div>
-      </div>
-    </div>
+      </li>
+    </ul>
 
   </div>
 </section>
