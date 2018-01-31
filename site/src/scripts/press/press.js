fdf52e103ee9483f44d6c58e5314ba736e3dbb8a
Blake Watkins <iamsoblake@gmail.com>
2018-01-30 16:19:38 -0800

    Add script changes for filtering

--- site/src/scripts/press/press.js
+++ site/src/scripts/press/press.js
@@ -4,11 +4,21 @@ import Carousel from './Carousel';
 
 if ($('body').hasClass('page-template-press')) {
   // Enable dropdown
-  $('.press-media-dropdown').on('click', function(ev) {
+  $('.press-media-dropdown').on('click', () => {
     $('.press-media-dropdown-container').toggle();
   });
 
   // Create carousel
-  const magCarousel = new Carousel('.press-media-mags', '.press-media-back', '.press-media-next');
+  const magCarousel = new Carousel(
+    '.press-media-mags',
+    '.press-media-back',
+    '.press-media-next'
+  );
+
+  magCarousel.setFilter(
+    '.press-media-mags-wrapper',
+    '.press-media-mag-item',
+    '.press-media-mag-filter'
+  );
 }
 
