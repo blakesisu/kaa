1f38beac98b400e725c6ab8d13b7f696eef71412
Blake Watkins <iamsoblake@gmail.com>
2018-02-01 09:46:29 -0800

    fix body class check when initializing project script

--- site/src/scripts/projects/projects.js
+++ site/src/scripts/projects/projects.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import Scroll from './Scroll';
 
 
-if ($('body').hasClass('page-template-projects')) {
+if ($('body').hasClass('page-template-projects') ||
+  $('body').hasClass('post-type-archive-projects')) {
   // Create leader objects
   $('.proj-projects__project').each((i, el) => {
     /* eslint-disable no-new */
