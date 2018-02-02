370caeaa37142432fea01daba83f10451755dd3e
Blake Watkins <iamsoblake@gmail.com>
2018-02-02 13:03:36 -0800

    Modyify script for remote dev machine

--- site/scripts/sync-machines.sh
+++ site/scripts/sync-machines.sh
@@ -6,18 +6,18 @@ NOW=`date +"%m_%d_%Y_%H_%M_%S"`
 DEVDIR="web/app/"
 DEVSITE="dev.kaadesigngroup.com"
 
-# REMOTEDEVDIR="web@107.170.244.77:/srv/www/kaa/current/web/app/"
-# REMOTEDEVSITE="107.170.244.77"
+REMOTEDEVSITE="107.170.244.77"
+REMOTEDEVDIR="web@107.170.244.77:/srv/www/kaa/current/web/app/"
 
-PRODDIR="web@104.236.139.224:/srv/www/kaa/current/web/app/"
 PRODSITE="104.236.139.224"
+PRODDIR="web@104.236.139.224:/srv/www/kaa/current/web/app/"
 
-STAGDIR="web@165.227.56.50:/srv/www/kaa/current/web/app/"
-STAGSITE="165.227.56.50"
+STAGESITE="165.227.56.50"
+STAGEDIR="web@165.227.56.50:/srv/www/kaa/current/web/app/"
 
 if [ $# -eq 0 ]; then
-  read -r -p "Which database do you want to reset? [dev/stage/prod] " DB_RESP
-  read -r -p "Which database do you want to sync from? [dev/stage/prod] " SYNC_RESP
+  read -r -p "Which database do you want to reset? [dev/stage/prod/remotedev] " DB_RESP
+  read -r -p "Which database do you want to sync from? [dev/stage/prod/remotedev] " SYNC_RESP
   TO=$DB_RESP
   FROM=$SYNC_RESP
 else
@@ -27,10 +27,18 @@ fi
 
 case "$TO-$FROM" in
   dev-prod) DIR="up";  FROMSITE=$DEVSITE;  FROMDIR=$DEVDIR;  TOSITE=$PRODSITE; TODIR=$PRODDIR; ;;
-  dev-stage)    DIR="up"   FROMSITE=$DEVSITE;  FROMDIR=$DEVDIR;  TOSITE=$STAGSITE; TODIR=$STAGDIR; ;;
+  dev-stage)    DIR="up"   FROMSITE=$DEVSITE;  FROMDIR=$DEVDIR;  TOSITE=$STAGESITE; TODIR=$STAGEDIR; ;;
   prod-dev) DIR="down" FROMSITE=$PRODSITE; FROMDIR=$PRODDIR; TOSITE=$DEVSITE;  TODIR=$DEVDIR; ;;
-  stage-dev)    DIR="down" FROMSITE=$STAGSITE; FROMDIR=$STAGDIR; TOSITE=$DEVSITE;  TODIR=$DEVDIR; ;;
-  *) echo "usage: $0 dev prod | dev stage | prod dev | prod stage" && exit 1 ;;
+  stage-dev)    DIR="down" FROMSITE=$STAGESITE; FROMDIR=$STAGEDIR; TOSITE=$DEVSITE;  TODIR=$DEVDIR; ;;
+
+  # New to try to work with remote dev
+  remotedev-dev)    DIR="down" FROMSITE=$REMOTEDEVSITE; FROMDIR=$REMOTEDEVDIR; TOSITE=$DEVSITE;  TODIR=$DEVDIR; ;;
+  dev-remotedev)    DIR="down" FROMSITE=$DEVSITE; FROMDIR=$DEVDIR; TOSITE=$REMOTEDEVSITE;  TODIR=$REMOTEDEVDIR; ;;
+
+  remotedev-prod)    DIR="down" FROMSITE=$REMOTEDEVSITE; FROMDIR=$REMOTEDEVDIR; TOSITE=$PRODSITE;  TODIR=$PRODDIR; ;;
+  prod-remotedev)    DIR="down" FROMSITE=$PRODSITE; FROMDIR=$PRODDIR; TOSITE=$REMOTEDEVSITE;  TODIR=$REMOTEDEVDIR; ;;
+
+  *) echo "usage: $0 dev prod | dev stage | prod dev | prod stage | dev remotedev | remotedev dev | remotedev prod | prod remotedev" && exit 1 ;;
 esac
 
 read -r -p "Reset the $TO database and sync from $FROM? [y/N] " response
