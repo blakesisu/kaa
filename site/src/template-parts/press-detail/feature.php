a7476e0d598abf73bf5fadbc5bf31687e0899449
Blake Watkins <iamsoblake@gmail.com>
2018-02-06 17:51:38 -0800

    initial templates and styles for press-details

---
+++ site/src/template-parts/press-detail/feature.php
@@ -0,0 +1,64 @@
+<section class="press-details">
+    <!-- Image Block -->
+    <!-- use php boolean logic here to determine whether to show placeholder or hero -->
+
+    <!-- placeholder -->
+    <div class="press-details-feature-placeholder">
+      <div class="press-details-placeholder-wrapper">
+        <picture class="press-details-picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg">
+          <img class="press-details-img" srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg" alt="">
+        </picture>
+      </div>
+    </div>
+
+    <!-- hero -->
+    <!-- <div class="press&#45;details&#45;feature&#45;hero"> -->
+    <!--   <div class="press&#45;details&#45;img&#45;wrapper"> -->
+    <!--     <picture class="press&#45;details&#45;picture"> -->
+    <!--       <source srcset="<?php bloginfo('template_url'); ?>/images/about/hero/about&#45;hero&#45;lg.jpg"> -->
+    <!--       <img class="press&#45;details&#45;img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg" alt=""> -->
+    <!--     </picture> -->
+    <!--   </div> -->
+    <!-- </div> -->
+
+
+    <!-- Content date, title, links -->
+    <div class="press-details-item press-details-text-feature press-details-dual">
+      <div class="press-details-text-wrapper">
+        <div class="press-details-text-header">
+          <h2>CALIFORNIA CONTEMPORARY: A Book Over 30 Years in the Making</h2>
+        </div>
+        <div class="press-details-text-description">
+          <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+        </div>
+        <a href="#" class="press-details-read-button">READ MORE</a>
+      </div>
+    </div>
+
+    <!-- Content optional picture -->
+    <div class="press-details-item press-details-dual">
+      <div class="press-details-img-wrapper">
+        <picture class="press-details-picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/press/mobile/dtran-sm.jpg" media="(max-width: 700px)">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/dtran.jpg">
+          <img class="press-details-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg" alt="">
+        </picture>
+      </div>
+    </div>
+
+    <!-- Content description -->
+    <div class="press-details-item press-details-text-aside press-details-single">
+      <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+    </div>
+
+    <!-- Additional narrative (light) -->
+    <div class="press-details-item press-details-text-aside press-details-single">
+      <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+    </div>
+
+    <!-- Additional narrative (dark) -->
+    <div class="press-details-item press-details-text-aside press-details-single press-details-dark">
+      <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+    </div>
+</section>
