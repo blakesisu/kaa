16a4c53ed3b80bab7cfeeb88934ca36ee635508f
Blake Watkins <iamsoblake@gmail.com>
2018-02-07 11:48:28 -0800

    Prep defining styles and templates for wp php logic

--- site/src/template-parts/press-detail/feature.php
+++ site/src/template-parts/press-detail/feature.php
@@ -3,35 +3,30 @@
     <!-- use php boolean logic here to determine whether to show placeholder or hero -->
 
     <!-- placeholder -->
-    <div class="press-details-feature-placeholder">
-      <div class="press-details-placeholder-wrapper">
-        <picture class="press-details-picture">
-          <source srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg">
-          <img class="press-details-img" srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details-hero-lg.jpg" alt="">
-        </picture>
-      </div>
-    </div>
-
-    <!-- hero -->
-    <!-- <div class="press&#45;details&#45;feature&#45;hero"> -->
-    <!--   <div class="press&#45;details&#45;img&#45;wrapper"> -->
+    <!-- <div class="press&#45;details&#45;placeholder"> -->
+    <!--   <div class="press&#45;details&#45;placeholder&#45;wrapper"> -->
     <!--     <picture class="press&#45;details&#45;picture"> -->
-    <!--       <source srcset="<?php bloginfo('template_url'); ?>/images/about/hero/about&#45;hero&#45;lg.jpg"> -->
-    <!--       <img class="press&#45;details&#45;img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg" alt=""> -->
+    <!--       <source srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details&#45;hero&#45;lg.jpg"> -->
+    <!--       <img class="press&#45;details&#45;img" srcset="<?php bloginfo('template_url'); ?>/images/details/hero/details&#45;hero&#45;lg.jpg" alt=""> -->
     <!--     </picture> -->
     <!--   </div> -->
     <!-- </div> -->
 
+    <!-- hero -->
+    <div class="press-details-hero">
+      <div class="press-details-hero-wrapper">
+        <picture class="press-details-picture">
+          <source srcset="<?php bloginfo('template_url'); ?>/images/about/hero/about-hero-lg.jpg">
+          <img class="press-details-img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/Book.jpg" alt="">
+        </picture>
+      </div>
+    </div>
 
     <!-- Content date, title, links -->
-    <div class="press-details-item press-details-text-feature press-details-dual">
-      <div class="press-details-text-wrapper">
-        <div class="press-details-text-header">
-          <h2>CALIFORNIA CONTEMPORARY: A Book Over 30 Years in the Making</h2>
-        </div>
-        <div class="press-details-text-description">
-          <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
-        </div>
+    <div class="press-details-item press-details-feature-meta press-details-dual">
+      <div class="press-details-feature-wrapper">
+        <p class="press-details-feature-date">APR 5, 2015</h2>
+        <h2 class="press-details-feature-header">CALIFORNIA CONTEMPORARY: A Book Over 30 Years in the Making</h2>
         <a href="#" class="press-details-read-button">READ MORE</a>
       </div>
     </div>
@@ -48,17 +43,107 @@
     </div>
 
     <!-- Content description -->
-    <div class="press-details-item press-details-text-aside press-details-single">
-      <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+    <!-- php boolean for feature content: give either dual or single-feature -->
+    <!-- <div class="press&#45;details&#45;item press&#45;details&#45;feature&#45;content press&#45;details&#45;dual"> -->
+    <div class="press-details-item press-details-feature-content press-details-single-feature">
+      <p>Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling</p>
+
+      <!-- php boolean for display -->
+      <!-- <a href="#" class="press&#45;details&#45;share&#45;button"> -->
+      <!--   <?php include(__DIR__ ."/../svgs/share.svg"); ?> Share -->
+      <!-- </a> -->
     </div>
 
     <!-- Additional narrative (light) -->
     <div class="press-details-item press-details-text-aside press-details-single">
-      <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+      <p>Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling</p>
     </div>
 
     <!-- Additional narrative (dark) -->
     <div class="press-details-item press-details-text-aside press-details-single press-details-dark">
-      <p>Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling.</p>
+      <p>Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling Situated on a hillside property on a southern flank of the Santa Monica Mountains, Tower Grove rises high above to capture breathtaking and privileged views of Los Angeles and the Pacific Ocean beyond.Explore the houses of founder Grant C. Kirkpatrick and KAA Design, and our love for nature, design and storytelling</p>
     </div>
+
+
+<?php
+  // $relatedProject1 = get_field('related_project_1');
+  // $relatedProject2 = get_field('related_project_2');
+  // $relatedArray = [$relatedProject1, $relatedProject2];
+?>
+
+<?php
+  // if ($relatedProject1) :
+?>
+
+  <div class="pd-related-articles__container">
+    <h1 class="pd-related-projects__header">Related Articles</h1>
+    <div class="pd-related-projects__dash"></div>
+
+    <div class="pd-related-projects__projects">
+      <?php
+        // foreach($relatedArray as $project) :
+      ?>
+        <?php
+          // override $post
+
+          // $post = $project;
+          // setup_postdata( $post );
+          // $hero = get_field('project_detail_hero');
+        ?>
+        <div class="pd-related-projects__project pd-related-project">
+          <?php
+            // echo the_permalink($post->ID);
+          ?>
+          <a href="#" class="pd-related-project__link">
+            <div class="pd-related-project__copy">
+              <?php
+                // echo the_title();
+              ?>
+              <h2 class="pd-related-project__header">HEADER</h2>
+            </div>
+            <p class="pd-related-project__more">More <?php include(__DIR__ ."/../svgs/chevron-right.svg"); ?></p>
+            <picture class="pd-related-project__picture">
+              <?php
+                // echo $hero['thumbnail'];
+              ?>
+              <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/dtran.jpg">
+              <img class="pd-related-project__img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/dtran.jpg" alt="">
+            </picture>
+          </a>
+        </div>
+
+        <div class="pd-related-projects__project pd-related-project">
+          <?php
+            // echo the_permalink($post->ID);
+          ?>
+          <a href="#" class="pd-related-project__link">
+            <div class="pd-related-project__copy">
+              <?php
+                // echo the_title();
+              ?>
+              <h2 class="pd-related-project__header">HEADER</h2>
+            </div>
+            <p class="pd-related-project__more">More <?php include(__DIR__ ."/../svgs/chevron-right.svg"); ?></p>
+            <picture class="pd-related-project__picture">
+              <?php
+                // echo $hero['thumbnail'];
+              ?>
+              <source srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/dtran.jpg">
+              <img class="pd-related-project__img" srcset="<?php bloginfo('template_url'); ?>/images/press/desktop/dtran.jpg" alt="">
+            </picture>
+          </a>
+        </div>
+
+
+        <?php
+          // wp_reset_postdata();
+        ?>
+      <?php
+        // endforeach;
+        ?>
+    </div>
+  </div>
+  <?php
+  // endif;
+  ?>
 </section>
