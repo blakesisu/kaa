772b896be2cb62549c7d787a6ac38b49073a99d8
Steven Waller <jstevenwaller@gmail.com>
2018-02-09 16:30:44 -0800

    Added video support to About hero

--- site/src/functions.php
+++ site/src/functions.php
@@ -9,6 +9,42 @@ function admin_style()
 
 add_action('admin_enqueue_scripts', 'admin_style');
 
+// https://github.com/lingtalfi/video-ids-and-thumbnails/blob/master/function.video.php
+function getVimeoId($url) {
+  if (preg_match('#(?:https?://)?(?:www.)?(?:player.)?vimeo.com/(?:[a-z]*/)*([0-9]{6,11})[?]?.*#', $url, $m)) {
+    return $m[1];
+  }
+  return false;
+}
+
+// https://github.com/lingtalfi/video-ids-and-thumbnails/blob/master/function.video.php
+function getYoutubeId($url) {
+  $parts = parse_url($url);
+  if (isset($parts['host'])) {
+    $host = $parts['host'];
+    if (
+      false === strpos($host, 'youtube') &&
+      false === strpos($host, 'youtu.be')
+    ) {
+      return false;
+    }
+  }
+  if (isset($parts['query'])) {
+    parse_str($parts['query'], $qs);
+    if (isset($qs['v'])) {
+      return $qs['v'];
+    }
+    else if (isset($qs['vi'])) {
+      return $qs['vi'];
+    }
+  }
+  if (isset($parts['path'])) {
+    $path = explode('/', trim($parts['path'], '/'));
+    return $path[count($path) - 1];
+  }
+  return false;
+}
+
 // limit posts per type
 add_action('pre_get_posts', 'be_change_event_posts_per_page');
 /**
@@ -19,6 +55,7 @@ add_action('pre_get_posts', 'be_change_event_posts_per_page');
  * @param object $query data
  *
  */
+
 function be_change_event_posts_per_page($query)
 {
     if ($query->is_main_query() && !is_admin() && is_post_type_archive('press')) {
--- site/src/template-parts/about/hero.php
+++ site/src/template-parts/about/hero.php
@@ -1,11 +1,43 @@
 <section class="about-grid about-hello-hero">
   <div class="about-hero">
-    <?php $image = get_field('about_hero'); ?>
-    <picture class="about-image-block__picture">
-      <source srcset="<?php echo $image['mobile']; ?>" media="(max-width: 700px)">
-      <source srcset="<?php echo $image['desktop']; ?>">
-      <img class="about-image-block__img" srcset="<?php echo $image['desktop']; ?>" alt="">
-    </picture>
+    <?php $type = get_field('about_hero_type'); ?>
+    <?php if ($type === 'image'): ?>
+      <?php $image = get_field('about_hero'); ?>
+      <picture class="about-image-block__picture">
+        <source srcset="<?php echo $image['mobile']; ?>" media="(max-width: 700px)">
+        <source srcset="<?php echo $image['desktop']; ?>">
+        <img class="about-image-block__img" srcset="<?php echo $image['desktop']; ?>" alt="">
+      </picture>
+    <?php else: ?>
+      <?php
+        $video = get_field('about_hero_video');
+        $videoID = null;
+        $videoSrc = null;
+
+        // If vimeo video
+        if (strpos($video, 'vimeo') !== false) {
+          $videoID = getVimeoId($video);
+          $videoSrc = 'https://player.vimeo.com/video/'.$videoID.'?title=0&byline=0&portrait=0';
+        // If youtube video
+        } else {
+          $videoID = getYoutubeId($video);
+          $videoSrc = 'https://www.youtube.com/embed/'.$videoID;
+        }
+      ?>
+      <?php if ($videoSrc): ?>
+        <div class="about__video-wrapper">
+          <iframe
+            src="<?php echo $videoSrc; ?>"
+            width="640"
+            height="360"
+            frameborder="0"
+            webkitallowfullscreen
+            mozallowfullscreen
+            allowfullscreen
+          ></iframe>
+        </div>
+      <?php endif; ?>
+    <?php endif; ?>
   </div>
 
 </section>
