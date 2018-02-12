35295ea2220966211c14cb83bb025523dd36198b
Steven Waller <jstevenwaller@gmail.com>
2018-02-12 14:33:43 -0800

    Added google analytics

--- site/src/header.php
+++ site/src/header.php
@@ -18,6 +18,18 @@
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/picturefill/3.0.0-beta1/picturefill.min.js"></script>
 
+  <!-- Google Analytics -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-107069118-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <!-- End Google Analytics -->
+
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
