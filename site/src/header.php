16a8a16ec3372c97791ac589dbce18793ad287b6
Steven Waller <jstevenwaller@gmail.com>
2018-02-12 12:41:18 -0800

    Added favicon

--- site/src/header.php
+++ site/src/header.php
@@ -9,7 +9,7 @@
   <meta name="keywords" content="">
   <meta http-equiv="content-language" content="en">
 
-  <link rel="icon" href="favicon.ico">
+  <link rel="icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
 
   <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
 
