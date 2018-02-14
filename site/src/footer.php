b3a337eab50f91a75755e18d25cdfff4e4b8c50a
Steven Waller <jstevenwaller@gmail.com>
2018-02-14 15:11:13 -0800

    Cache bust

--- site/src/footer.php
+++ site/src/footer.php
@@ -1,8 +1,8 @@
   <?php include("template-parts/common/site-footer.php"); ?>
 
-  <script src="<?php echo get_stylesheet_directory_uri().'/js/manifest.js?v=1.0.5'; ?>"></script>
-  <script src="<?php echo get_stylesheet_directory_uri().'/js/vendor.js?v=1.0.5'; ?>"></script>
-  <script src="<?php echo get_stylesheet_directory_uri().'/js/scripts.js?v=1.0.5'; ?>"></script>
+  <script src="<?php echo get_stylesheet_directory_uri().'/js/manifest.js?v=1.0.6'; ?>"></script>
+  <script src="<?php echo get_stylesheet_directory_uri().'/js/vendor.js?v=1.0.6'; ?>"></script>
+  <script src="<?php echo get_stylesheet_directory_uri().'/js/scripts.js?v=1.0.6'; ?>"></script>
 
   <?php wp_footer(); ?>
 </body>
