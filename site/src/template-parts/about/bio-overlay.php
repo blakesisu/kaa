c82aace1f8b58407c2b8a4e0510a14aa8b55f5e2
Steven Waller <jstevenwaller@gmail.com>
2018-02-15 14:11:42 -0800

    Using new leader WP content

--- site/src/template-parts/about/bio-overlay.php
+++ site/src/template-parts/about/bio-overlay.php
@@ -1,44 +1,56 @@
-<?php if( have_rows('leadership_repeater') ): ?>
-  <?php while( have_rows('leadership_repeater') ): the_row(); ?>
+<?php if( have_rows('leadership_leaders') ): ?>
+  <?php while( have_rows('leadership_leaders') ): the_row(); ?>
     <?php
-      $post = get_sub_field('leadership_leader');
-      setup_postdata( $post );
+      $name = get_sub_field('leader_name');
+      $id = preg_replace("/[^\w]+/", "-", $name);
+      $title = get_sub_field('leader_title');
+      $image = get_sub_field('leader_image');
+      $bio = get_sub_field('leader_bio');
     ?>
-      <section id="<?php the_ID(); ?>" class="ab-bio">
-        <button class="ab-bio__close-btn"><?php include(__DIR__ ."/../svgs/close.svg"); ?></button>
-        <div class="ab-bio__header ab-bio__header--mobile">
-          <h1 class="ab-bio__name"><?php the_title(); ?></h1>
-          <h2 class="ab-bio__title"><?php the_field('leader_title'); ?></h2>
-          <div class="ab-bio__dash"></div>
-        </div>
-        <div class="ab-bio__image-container">
-          <?php $image = get_field('leader_image'); ?>
-          <picture class="ab-bio__picture">
-            <source srcset="<?php echo $image['Grid']; ?>" media="(max-width: 700px)">
-            <source srcset="<?php echo $image['bio']; ?>">
-            <img class="ab-bio__img" srcset="<?php echo $image['bio']; ?>" alt="">
-          </picture>
-        </div>
-        <div class="ab-bio__text-container">
+    <section id="<?php echo $id; ?>" class="ab-bio">
+      <button class="ab-bio__close-btn"><?php include(__DIR__ ."/../svgs/close.svg"); ?></button>
+      <div class="ab-bio__header ab-bio__header--mobile">
+        <h1 class="ab-bio__name"><?php echo $name; ?></h1>
+        <h2 class="ab-bio__title"><?php echo $title; ?></h2>
+        <div class="ab-bio__dash"></div>
+      </div>
+      <div class="ab-bio__image-container">
+        <picture class="ab-bio__picture">
+          <source srcset="<?php echo $image['Grid']; ?>" media="(max-width: 700px)">
+          <source srcset="<?php echo $image['bio']; ?>">
+          <img class="ab-bio__img" srcset="<?php echo $image['bio']; ?>" alt="">
+        </picture>
+      </div>
+      <div class="ab-bio__text-container">
+        <div class="ab-bio__text-wrapper">
           <div class="ab-bio__header ab-bio__header--desktop">
-            <h1 class="ab-bio__name"><?php the_title(); ?></h1>
-            <h2 class="ab-bio__title"><?php the_field('leader_title'); ?></h2>
+            <h1 class="ab-bio__name"><?php echo $name; ?></h1>
+            <h2 class="ab-bio__title"><?php echo $title; ?></h2>
             <div class="ab-bio__dash"></div>
           </div>
           <div class="ab-bio__scroller">
             <div class="ab-bio__copy">
-              <?php the_field('leader_bio'); ?>
+              <?php echo $bio; ?>
             </div>
-            <!-- <div class="ab-bio__articles">
-              <h3 class="ab-bio__articles-header">Contributed Articles</h3>
-              <div class="ab-bio__article">
-                <h4 class="ab-bio__article-header"><a href="#">Historic Preservation Today <?php include(__DIR__ ."/../svgs/chevron-right.svg"); ?></a></h4>
-                <p class="ab-bio__article-description">Ken Breisch, former Director of USC's Graduate Program in Historic Preservation, speaks on the history and philosophy of the preservation movement as it has evolved during the past century and its relevance to Southern California's treasures.</p>
+            <?php if( have_rows('leader_articles') ): ?>
+              <div class="ab-bio__articles">
+                <h3 class="ab-bio__articles-header">Contributed Articles</h3>
+                <?php while( have_rows('leader_articles') ): the_row(); ?>
+                  <?php
+                    $post = get_sub_field('leader_article');
+                    setup_postdata( $post );
+                  ?>
+                  <div class="ab-bio__article">
+                    <h4 class="ab-bio__article-header"><a href="<?php echo the_permalink($post->ID); ?>"><?php echo the_title(); ?> <?php include(__DIR__ ."/../svgs/chevron-right.svg"); ?></a></h4>
+                    <p class="ab-bio__article-description">Ken Breisch, former Director of USC's Graduate Program in Historic Preservation, speaks on the history and philosophy of the preservation movement as it has evolved during the past century and its relevance to Southern California's treasures.</p>
+                  </div>
+                  <?php wp_reset_postdata(); ?>
+                <?php endwhile; ?>
               </div>
-            </div> -->
+            <?php endif; ?>
           </div>
         </div>
-      </section>
-    <?php wp_reset_postdata(); ?>
+      </div>
+    </section>
   <?php endwhile; ?>
 <?php endif; ?>
--- site/src/template-parts/about/family.php
+++ site/src/template-parts/about/family.php
@@ -1,43 +1,42 @@
 <section class="about-grid about-family">
-
-    <div class="about-family-main">
-      <div class="about-family-person">
-        <div class="about-text-flex-column">
-          <h2 class="about-family-headline"><?php the_field('leadership_headline'); ?></h2>
-          <div class="about-family__dash"></div>
-        </div>
+  <div class="about-family-main">
+    <div class="about-family-person">
+      <div class="about-text-flex-column">
+        <h2 class="about-family-headline"><?php the_field('leadership_headline'); ?></h2>
+        <div class="about-family__dash"></div>
       </div>
-
-      <?php if( have_rows('leadership_repeater') ): ?>
-        <?php while( have_rows('leadership_repeater') ): the_row(); ?>
-          <?php
-            $post = get_sub_field('leadership_leader');
-            setup_postdata( $post );
-          ?>
-
-          <div class="about-family-person">
-            <a class="about-family-link" href="#<?php the_ID(); ?>">
-              <div class="about-family-copy">
-                <div class="about-family-bio">
-                  <p class="about-family-bio-text">View Bio</p>
-                </div>
-              </div>
-
-              <?php $image = get_field('leader_image'); ?>
-              <picture class="about-image-block__picture">
-                <source srcset="<?php echo $image['Grid']; ?>">
-                <img class="about-image-block__img" srcset="<?php echo $image['Grid']; ?>" alt="">
-              </picture>
-              <div class="about-family-background"></div>
-              <div class="about-family-description">
-                <h3 class="about-family-name"><?php the_title(); ?></h3>
-                <p class="about-family-title"><?php the_field('leader_title'); ?></p>
-              </div>
-            </a>
-          </div>
-          <?php wp_reset_postdata(); ?>
-        <?php endwhile; ?>
-      <?php endif; ?>
-
     </div>
+
+    <?php if( have_rows('leadership_leaders') ): ?>
+      <?php while( have_rows('leadership_leaders') ): the_row(); ?>
+        <?php
+          $name = get_sub_field('leader_name');
+          $id = preg_replace("/[^\w]+/", "-", $name);
+          $title = get_sub_field('leader_title');
+          $image = get_sub_field('leader_image');
+        ?>
+
+        <div class="about-family-person">
+          <a class="about-family-link" href="#<?php echo $id; ?>">
+            <div class="about-family-copy">
+              <div class="about-family-bio">
+                <p class="about-family-bio-text">View Bio</p>
+              </div>
+            </div>
+
+            <picture class="about-image-block__picture">
+              <source srcset="<?php echo $image['Grid']; ?>">
+              <img class="about-image-block__img" srcset="<?php echo $image['Grid']; ?>" alt="">
+            </picture>
+            <div class="about-family-background"></div>
+            <div class="about-family-description">
+              <h3 class="about-family-name"><?php echo $name; ?></h3>
+              <p class="about-family-title"><?php echo $title; ?></p>
+            </div>
+          </a>
+        </div>
+      <?php endwhile; ?>
+    <?php endif; ?>
+
+  </div>
 </section>
