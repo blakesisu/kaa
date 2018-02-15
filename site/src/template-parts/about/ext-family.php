9a6a23500c73f30f521fd6d3d025a3366ba5d6a4
Steven Waller <jstevenwaller@gmail.com>
2018-02-15 15:19:24 -0800

    Using pictures for extended family

--- site/src/template-parts/about/ext-family.php
+++ site/src/template-parts/about/ext-family.php
@@ -3,12 +3,20 @@
     <h1 class="about-family-header"><?php the_field('family_headline'); ?></h1>
   </div>
 
-  <ul class="about-ext-family-members">
+  <ul class="about-ext-family__list">
     <?php if( have_rows('family_members') ): ?>
       <?php while( have_rows('family_members') ): the_row(); ?>
-      <li class="about-ext-family-container">
-        <h3 class="about-ext-family-name"><?php the_sub_field('name'); ?></h3>
-        <span class="about-ext-family-title"><?php the_sub_field('title'); ?></span>
+      <?php
+        $name = get_sub_field('name');
+        $title = get_sub_field('title');
+        $image = get_sub_field('image');
+      ?>
+      <li class="about-ext-family__item">
+        <div class="about-ext-family__text-container">
+          <h3 class="about-ext-family__name"><?php echo $name; ?></h3>
+          <span class="about-ext-family__title"><?php echo $title; ?></span>
+        </div>
+        <img class="about-ext-family__img" srcset="<?php echo $image; ?>" alt="">
       </li>
       <?php endwhile; ?>
     <?php endif; ?>
